c1ea83190fdcafec41f54abc58be2babb008f34c
rpaulnelsonii@gmail.com <rpaulnelsonii@gmail.com>
2024-06-12 23:29:03 -0700

    Fixed deployment bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header(props) {
     return (
     <header className="header-container"> 
     <h1>Ronald "Paulee" Nelson</h1>
-        <img src="https://images.unsplash.com/photo-1564410267841-915d8e4d71ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW90aXZhdGlvbnxlbnwwfHwwfHx8MA%3D%3D" alt="Profile Picture"></img>
+        <img src="https://images.unsplash.com/photo-1564410267841-915d8e4d71ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW90aXZhdGlvbnxlbnwwfHwwfHx8MA%3D%3D" alt="Profile Quote"></img>
         {props.children}
     </header>
 
